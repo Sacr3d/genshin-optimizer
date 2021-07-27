40125e1b32897f90ab9f2398041163d356508d3a
frzyc <frzyc@users.noreply.github.com>
2021-07-27 11:14:58 -0400

    weaponKey Record

--- src/Types/weapon.d.ts
+++ src/Types/weapon.d.ts
@@ -1,11 +1,12 @@
 import { WeaponData } from "../../pipeline";
 import { SubstatKey } from "./artifact";
 import { DocumentSection } from "./character";
-import { Rarity, WeaponTypeKey } from "./consts";
+import { Rarity, WeaponKey, WeaponTypeKey } from "./consts";
 import { BonusStats } from "./stats";
 import { IConditionals } from "./IConditional";
 
-export type IWeaponSheets = Dict<string, IWeaponSheet>
+
+export type IWeaponSheets = Record<WeaponKey, IWeaponSheet>
 
 export interface IWeaponSheet extends WeaponData {
   img: string;
